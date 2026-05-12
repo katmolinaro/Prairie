4ccf73bbff7b0e7904cca58239edcc96c66d3b0d
katmolinaro <katiamolinaro1311@gmail.com>
2026-05-12 18:01:21 +0200

    script

--- script/import_data.R
+++ script/import_data.R
@@ -17,6 +17,25 @@ list_sp <- read_excel("raw/Relevés_CISE_Prairie",
 #clean data ----
 #:)
 
+##Supprimer les deux dernière colonnes du premier tableau ----
+
+prairie_sp_clean <- prairie_sp %>%
+  select(-c((ncol(prairie_sp) - 1):ncol(prairie_sp)))
+
+## Remplacer les NA par des 0 ----
+prairie_sp_clean <- prairie_sp_clean %>%
+  mutate_all(~ replace_na(., 0))
+
+## Renommer les colonnes du second tableau ----
+
+noms_propres <- c("Annees", "Parcelles", "Hauteur_max_(cm)", "Hauteur_moyenne_(cm)","%_sol_nu", "	Mode_de_gestion", "Humidité_1", "Humidité_2", "Humidité_3", "Taupe" )
+
+prairie_sp_clean <- prairie_sp_clean %>%
+  set_names(noms_propres)
+
+
+## 
+
 
 
 
