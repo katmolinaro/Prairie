0d6e7639e01e05c132dfc3a33d19015a186753eb
katmolinaro <katiamolinaro1311@gmail.com>
2026-05-13 16:19:01 +0200

    traits

--- script/traits.R
+++ script/traits.R
@@ -1,5 +1,16 @@
 #Analyses Traits
 
+available_tr8
+traits_eco
+
+#test avec un vecteur
+
+list <- pull(.data = list_sp, var = -1)
+list
+
+ell <- c("ell_light_uk", "ell_moist_uk", "ell_pH_uk", "ell_N", "ell_S")
+
+test <- tr8(species_list = c("Poa annua","Holcus lanatus"), download_list = ell, allow_persistent = T)
 
 
 #Extract all species data ----
